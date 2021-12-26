e5281c7fd5824acc902d6600c40cb30ede1d812d
Your Name <you@example.com>
2021-12-26 14:28:16 +0530

    favicon and manifest.json added and updated footer  Github repository URL

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer>
-            <a href="https://github.com/munzir-dev" target="_blank" rel="noreferrer">Designed & Built by Muhammed Mundir</a>
+            <a href="https://github.com/mundir-dev/portfolio" target="_blank" rel="noreferrer">Designed & Built by Muhammed Mundir</a>
         </footer>
     )
 }
